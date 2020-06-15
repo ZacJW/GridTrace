c2ccc59587903e0ef5c5c75b88f872161ae5bf96
ZacJW <11651573+ZacJW@users.noreply.github.com>
2020-06-15 18:09:11 +0100

    Changed moment to luxon, added live chart updates

--- gridsense/static/update-chart-cell-voltage.js
+++ gridsense/static/update-chart-cell-voltage.js
@@ -9,7 +9,9 @@ var chart_config = {type: 'line',
                         scales: {
                             xAxes: [{
                                 type: "time",
-                                display: true
+                                display: true,
+                                time: {
+                                }
                             }],
                             yAxes: [{
                                 display: true,
@@ -23,37 +25,102 @@ var chart_config = {type: 'line',
                         }
                     }
                 }
-var chart_cell_voltage = new Chart(ctx, chart_config)
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function(){
-    if (this.readyState == 4 && this.status == 200){
-        var response = JSON.parse(this.responseText);
-        var datasets = {};
-        for (data of response){
-            for (reading of data[1]){
-                if (!(reading[0] in datasets)){
-                    datasets[reading[0]] = {data: []};
-                }
-                datasets[reading[0]].data.push({x: moment(data[0]), y: reading[1]/1000})
+var chart_cell_voltage = new Chart(ctx, chart_config);
+
+var load_interval = setInterval(load_data, 2000);
+var isLoaded = false;
+
+function load_data(){
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if (!isLoaded && this.readyState == 4 && this.status == 200){
+            var response = JSON.parse(this.responseText);
+            if (response.length == 0){
+                return;
             }
+            var datasets = build_datasets(response);
+            var last_moment = luxon.DateTime.fromHTTP(response[response.length-1][0])
+            datasets.sort(compare_datasets);
+            colour_datasets(datasets);
+            chart_cell_voltage.data.datasets = datasets;
+            chart_cell_voltage.data.last_moment = last_moment;
+            chart_cell_voltage.update();
+            clearInterval(load_interval);
+            isLoaded = true;
+            setInterval(live_update, 10000);
         }
-        for (set in datasets){
-            datasets[set].label = 'Cell ' + set;
-        }
-        datasets = Object.values(datasets);
-        for (set in datasets){
-            datasets[set].borderColor = 'hsl(' + parseInt(set / datasets.length * 360) + ', 65%, 65%)';
-            datasets[set].backgroundColor = 'hsl(' + parseInt(set / datasets.length * 360) + ', 65%, 65%)';
-            datasets[set].fill = false;
-            chart_config.data.datasets.push(datasets[set])
-        }
-        chart_cell_voltage.update()
-        
     }
-};
-//var start = moment().subtract(1, 'day');
-//var end = moment();
-//xhttp.open("GET", "/chart-api/historic/voltage?start=" + encodeURIComponent(start.format())
-//                 + "&end=" + encodeURIComponent(end.format()));
-xhttp.open("GET", "/chart-api/live/voltage?hours=24");
-xhttp.send();
+    xhr.open("GET", "/chart-api/live/voltage?hours=24");
+    xhr.send();
+}
+
+function build_datasets(response){
+    var datasets = [];
+    var cell_id_map = {};
+    for (time_point of response){
+        var time = luxon.DateTime.fromHTTP(time_point[0]);
+        for (cell_reading of time_point[1]){
+            var cell_id = cell_reading[0];
+            var volts = cell_reading[1]/1000;
+            if (!(cell_id in cell_id_map)){
+                cell_id_map[cell_id] = datasets.length;
+                datasets.push({data: []});
+            }
+            datasets[cell_id_map[cell_id]].data.push({x: time, y: volts})
+        }
+    }
+    for (cell_id in cell_id_map){
+        datasets[cell_id_map[cell_id]].label = 'Cell ' + cell_id;
+        datasets[cell_id_map[cell_id]].cell_id = cell_id;
+    }
+    return datasets;
+}
+
+function compare_datasets(a, b){
+    return a.cell_id - b.cell_id;
+}
+
+function colour_datasets(datasets){
+    for (set in datasets){
+        datasets[set].borderColor = 'hsl(' + parseInt(set / datasets.length * 360) + ', 65%, 65%)';
+        datasets[set].backgroundColor = 'hsl(' + parseInt(set / datasets.length * 360) + ', 65%, 65%)';
+        datasets[set].fill = false;
+    }
+}
+
+function live_update(){
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200){
+            var response = JSON.parse(this.responseText);
+            var datasets = build_datasets(response);
+            var last_moment = luxon.DateTime.fromHTTP(response[response.length-1][0])
+            if (this.last_moment != chart_cell_voltage.data.last_moment){
+                return;
+            }
+            for (new_dataset of datasets){
+                merge_dataset(new_dataset);
+            }
+            colour_datasets(chart_cell_voltage.data.datasets);
+            chart_cell_voltage.data.last_moment = last_moment;
+            chart_cell_voltage.update();
+        }
+    }
+    
+    xhr.open("GET", "/chart-api/historic/voltage?start=" + encodeURIComponent(chart_cell_voltage.data.last_moment.toISO()));
+    xhr.last_moment = chart_cell_voltage.data.last_moment;
+    xhr.send();
+}
+
+function merge_dataset(new_dataset){
+    var merged = false;
+    for (var i = 0; i < chart_cell_voltage.data.datasets.length; i++){
+        if (new_dataset.cell_id == chart_cell_voltage.data.datasets[i].cell_id){
+            chart_cell_voltage.data.datasets[i].data = chart_cell_voltage.data.datasets[i].data.concat(new_dataset.data.slice(1))
+            merged = true;
+        }
+    }
+    if (!merged){
+        chart_cell_voltage.data.datasets.push(new_dataset);
+    }
+}
