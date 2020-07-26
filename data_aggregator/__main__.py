61bcb1d8acee739e1056dd548d33a39fd5170d80
ZacJW <11651573+ZacJW@users.noreply.github.com>
2020-07-26 15:44:13 +0100

    Added database support to data_aggregator

--- data_aggregator/__main__.py
+++ data_aggregator/__main__.py
@@ -1,7 +1,9 @@
 from ssdpy import SSDPClient
 client = SSDPClient()
 import threading, time
+import psycopg2, psycopg2.sql
 
+db_details = {"dbname":"gridsense", "user":"data_aggregator", "password":"ErL8xakHT4rxUpUG", "host":"localhost"}
 SSDP_WAIT_PERIOD = 10 #seconds
 SSDP_SEARCH_TARGET = "urn:grid-sense:service:data-probe:1"
 SSDP_MX = 3 #seconds
@@ -23,6 +25,7 @@ class Data_Probe(threading.Thread):
         self.ttl = time.time() + DATA_PROBE_TTL
         self.stop_event = threading.Event()
         self.lock = threading.Lock()
+        self.conn = psycopg2.connect(**db_details)
         super().__init__()
 
     def refresh(self, ip_address, port, location):
@@ -34,38 +37,55 @@ class Data_Probe(threading.Thread):
 
     def run(self):
         import requests, defusedxml.ElementTree
+        from collections import namedtuple
         while not self.stop_event.isSet():
-            with self.lock:
-                ip_address = self.ip_address
-                port = self.port
-                location = self.location
-            if port == 80:
-                protocol = "http://"
-            else:
-                protocol = "https://"
             try:
+                with self.lock:
+                    ip_address = self.ip_address
+                    port = self.port
+                    location = self.location
+                if port == 80:
+                    protocol = "http://"
+                else:
+                    protocol = "https://"
                 schema_response = requests.get(protocol + ip_address + "/" + location)
-            except requests.ConnectionError:
-                self.stop_event.wait(timeout=WORKER_RETRY_PERIOD)
-                continue
-            if schema_response.ok:
-                schema = defusedxml.ElementTree.fromstring(schema_response.text)
-            else:
-                self.stop_event.wait(timeout=WORKER_RETRY_PERIOD)
-                continue
+                if schema_response.ok:
+                    schema = defusedxml.ElementTree.fromstring(schema_response.text)
+                else:
+                    self.stop_event.wait(timeout=WORKER_RETRY_PERIOD)
+                    continue
 
-            data_url = protocol + ip_address + "/" + schema[2][2].text
-            try:
+                data_url = protocol + ip_address + "/" + schema[2][2].text
                 data_response = requests.get(data_url)
-            except requests.ConnectionError:
-                self.stop_event.wait(timeout=WORKER_RETRY_PERIOD)
-                continue
-            if data_response.ok:
+                if data_response.ok and data_response.headers.get("Content-type", "").startswith("application/json"):
+                    with print_lock:
+                        print(usn, data_response.json())
+                    cur = self.conn.cursor()
+                    for table, data in data_response.json().items():
+                        columns = [column["name"] for column in data["columns"]]
+                        query = psycopg2.sql.SQL("INSERT INTO {table} ({columns}) VALUES ({data})").format(
+                            table=psycopg2.sql.Identifier(table),
+                            columns=psycopg2.sql.SQL(", ").join(map(psycopg2.sql.Identifier, columns)),
+                            data=psycopg2.sql.SQL(", ").join(psycopg2.sql.Placeholder() * len(columns))
+                        )
+                        for datapoint in data["values"]:
+                            try:
+                                cur.execute(query, datapoint)
+                            except Exception as e:
+                                self.conn.rollback()
+                                with print_lock:
+                                    print(e)
+                                self.stop_event.wait(timeout=WORKER_RETRY_PERIOD)
+                                continue
+                    self.conn.commit()
+                    self.stop_event.wait(timeout=WORKER_REQUEST_PERIOD)
+                else:
+                    self.stop_event.wait(timeout=WORKER_RETRY_PERIOD)
+            except Exception as e:
                 with print_lock:
-                    print(usn, data_response.json())
-                self.stop_event.wait(timeout=WORKER_REQUEST_PERIOD)
-            else:
+                    print(e)
                 self.stop_event.wait(timeout=WORKER_RETRY_PERIOD)
+        self.conn.close()
 
 
     def is_expired(self):
