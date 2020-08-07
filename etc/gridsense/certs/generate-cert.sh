66fbb5bdfb03d1697d200b6f1ac5822726ebaced
ZacJW <11651573+ZacJW@users.noreply.github.com>
2020-08-07 23:20:22 +0100

    Added SSL certificate config and helper script

---
+++ etc/gridsense/certs/generate-cert.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+if [ $(whoami) != "root" ]
+then
+    echo "Run as root"
+    exit 1
+fi
+cd /etc/gridsense/certs
+openssl req -config webserver.conf -new -x509 -sha256 -newkey rsa:2048 -nodes -keyout webserver.key -days 365 -out webserver.crt
+chown :www-data webserver.key webserver.crt
+chmod g+r webserver.key webserver.crt
+exit 0
